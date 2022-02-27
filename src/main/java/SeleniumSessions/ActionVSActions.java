cd1625c06e22e5c9a9106e495a7fa91a0d297f33
microblur <microblur@sina.cn>
2022-02-27 23:16:29 +1300

    add screeshots

--- src/main/java/SeleniumSessions/ActionVSActions.java
+++ src/main/java/SeleniumSessions/ActionVSActions.java
@@ -1,2 +1,29 @@
-package SeleniumSessions;public class ActionVSActions {
+package SeleniumSessions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionVSActions {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://automationpractice.com/index.php");
+
+        driver.manage().window().maximize();
+        WebElement features = driver.findElement(By.xpath("//a[@title='Women']"));
+
+        Actions act = new Actions(driver);
+        //act.moveToElement(features).perform();
+        Action action = act.moveToElement(features).build();
+        action.perform();
+
+        Thread.sleep(3000);
+        driver.quit();
+
+    }
 }
--- src/main/java/SeleniumSessions/CaptureScreenshots.java
+++ src/main/java/SeleniumSessions/CaptureScreenshots.java
@@ -1,2 +1,40 @@
-package SeleniumSessions;public class CaptureScreenshots {
+package SeleniumSessions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CaptureScreenshots {
+    public static void main(String[] args) throws IOException {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+        TakesScreenshot ts =  driver;
+        File src=ts.getScreenshotAs(OutputType.FILE);
+        File target = new File("./screenshots/homepage.png");
+        FileUtils.copyFile(src,target);
+
+        WebElement section = driver.findElement(By.xpath("//div[@class='category-grid home-page-category-grid']"));
+        File srcSection = section.getScreenshotAs(OutputType.FILE);
+        File trgSection = new File("./screenshots/featured.png");
+        FileUtils.copyFile(srcSection,trgSection);
+
+        WebElement img = driver.findElement(By.xpath("//img[@alt='nopCommerce demo store']"));
+
+        File srcImg = img.getScreenshotAs(OutputType.FILE);
+        File trgImg = new File("./screenshots/image.png");
+        FileUtils.copyFile(srcImg,trgImg);
+
+
+        driver.close();
+    }
 }
